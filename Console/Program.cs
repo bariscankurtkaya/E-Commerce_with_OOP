cc7428ef96c3316741ca676f45f85c405f040661
Barışcan Kurtkaya <33360380+bariscankurtkaya@users.noreply.github.com>
2021-02-13 19:42:10 +0300

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -17,18 +17,14 @@ namespace ConsoleUI
                 string brandName = productsManager.GetCarBrand(product.BrandId);
                 string colorName = productsManager.GetCarColor(product.ColorId);
                 Console.WriteLine(
-                    product.ProductId + ": " + 
-                    "Marka: " + brandName +
-                    " |Renk: " + colorName +
-                    " |Yıl: " + product.ModelYear +
-                    " |Günlük Ücretler:" + product.DailyPrice +
-                    " |Özellikleri: " + product.Description
+                    "{0}: Marka: {1}  --- Renk: {2} --- Yıl:{3}  --- Günlük Ücretler:{4} --- Özellikleri: {5}"
+                    , product.ProductId, brandName, colorName, product.ModelYear, product.DailyPrice, product.Description
                     );
             }
 
-            Console.WriteLine(productsManager.GetCarColor(3));
+            Console.WriteLine("3.Arabanın rengi: " +productsManager.GetCarColor(3));
             Products product1 = productsManager.GetById(5);
-            Console.WriteLine(product1.BrandId);
+            Console.WriteLine("5.Arabanın markası: " +product1.BrandId);
         }
     }
 }
